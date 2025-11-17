57ca14744b9fdc6c9e5ca726357b9f322b56dc5d
lordimagin <marc.gomis07@gmail.com>
2025-11-17 21:47:59 +0100

    Anotaciones ejercicios

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -10,26 +10,32 @@ find_first_sum(nums, goal)  # [2, 3]
 from os import system
 if system("clear") != 0: system("cls")
 
-# def find_first_sum(nums, goal):
-#   # early return, una validación rápida
-#   if len(nums) == 0: return None
-
-#   for i in range(len(nums)):
-#     for j in range(i + 1, len(nums)):
-#       if nums[i] + nums[j] == goal:
-#         return [i, j]
-
-#   return None # no se encontró ninguna combinación
-
 def find_first_sum(nums, goal):
-  seen = {} # diccionario para guardar el numero y su índice
+  # early return, una validación rápida
+  if len(nums) == 0: return None # Si la lista está vacía (osea el leng que sea 0), devuélveme un "None"
 
-  for index, value in enumerate(nums):
-    missing = goal - value
-    if missing in seen: return [seen[missing], index]
-    seen[value] = index # guardar el número actual a los vistos, porque no hemos encontrado la combinación
+  for i in range(len(nums)): # Bucle para ir en vertical, donde 'i' es la posición de la lista.
+    for j in range(i + 1, len(nums)): # Bucle para ir en horizontal, donde nunca compara un número consigo mismo y tampoco repite combinaciones ya probadas.
+      # | i | j valores | pares que compara |
+      # | - | --------- | ----------------- |
+      # | 0 | 1, 2      | (3,5), (3,7)      |
+      # | 1 | 2         | (5,7)             |
+      # | 2 | —         | no entra          | 
 
-  return None
+      if nums[i] + nums[j] == goal:
+        return [i, j]
+
+  return None # no se encontró ninguna combinación
+
+# def find_first_sum(nums, goal):
+#   seen = {} # diccionario para guardar el numero y su índice
+
+#   for index, value in enumerate(nums):
+#     missing = goal - value
+#     if missing in seen: return [seen[missing], index]
+#     seen[value] = index # guardar el número actual a los vistos, porque no hemos encontrado la combinación
+
+#   return None
 
 nums = [4, 5, 6, 2]
 goal = 8
